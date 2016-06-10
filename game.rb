cf0ff708125630fd147a4681a54ee0262c1fe2c2
Nicolas Pavon <nicolaspavon33gmail.com>
2016-06-10 15:03:05 -0300

    pre alpha, added nothing

--- game.rb
+++ game.rb
@@ -5,23 +5,48 @@ class GameWindow < Gosu::Window
     super(1204, 598)
     self.caption = 'Watch Out!!'
     @background = Gosu::Image.new(self, 'images/fondo.png', false)
-
+    @message = Gosu::Image.from_text(self, 'Press "o" to mute', Gosu.default_font_name, 30)
     @x = 500
-    @y = 300
+    @y = 390
     @draws = 0
     @buttons_down = 0
 
     @music = Gosu::Song.new(self, 'sounds/main.ogg')
     @music.volume = 0.5
     @music.play(true)
+
+    @player = Gosu::Image.new(self, 'images/nigel.png', false)
+
+    @variable
   end
 
   def update
     @x -= 3 if button_down?(Gosu::KbLeft)
     @x += 3 if button_down?(Gosu::KbRight)
-    @y -= 3 if button_down?(Gosu::KbUp)
-    @y += 3 if button_down?(Gosu::KbDown)
+    #@y -= 3 if button_down?(Gosu::KbUp)
+    #@y += 3 if button_down?(Gosu::KbDown)
+
+    @music.stop if button_down?(Gosu::KbO)
+
+    if button_down?(Gosu::KbLeft)
+      @player = Gosu::Image.new(self, 'images/nigel.png', false)
+      @variable = 1
+    elsif button_down?(Gosu::KbRight)
+      @variable = 2
+      @player = Gosu::Image.new(self, 'images/nigel_b.png', false)
     end
+
+    if button_down?(Gosu::KbSpace)
+      if @variable == 1
+        @x -= 6
+        @player = Gosu::Image.new(self, 'images/nigelmoviendose.png', false)
+
+      elsif @variable == 2
+        @x += 6
+        @player = Gosu::Image.new(self, 'images/nigelmoviendose_b.png', false)
+      end
+    end
+  end
 
   def button_down(id)
     close if id == Gosu::KbEscape
@@ -41,16 +66,8 @@ class GameWindow < Gosu::Window
     @scene_ready ||= true
     @background.draw(0, 0, 0)
     @draws += 1
-
-    if button_down?(Gosu::KbLeft)
-      @message = Gosu::Image.new(self, 'images/nigel.png', false)
-    elsif button_down?(Gosu::KbRight)
-      @message = Gosu::Image.new(self, 'images/nigel_b.png', false)
-    else
-      @message = Gosu::Image.new(self, 'images/nigel_b.png', false)
-    end
-
-    @message.draw(@x, @y, 0)
+    @message.draw(10, 10, 1)
+    @player.draw(@x, @y, 0)
 
   end
 end
